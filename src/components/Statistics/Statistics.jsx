0061b064eff519dedbacdb8c04dc8256b5b96e03
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2023-08-07 12:25:39 +0300

    fix homework 1

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,14 +2,14 @@ import { StatList, Wrapper, createRendomColor } from './Statistics.styled';
 
 export const Statistics = ({ title, stats }) => {
   return (
-    <Wrapper className="statistics">
-      {title ? <h2 className="title">Upload stats</h2> : ''}
+    <Wrapper>
+      {title ? <h2>{title}</h2> : ''}
 
-      <StatList className="stat-list">
+      <StatList>
         {stats.map(({ id, label, percentage }) => (
           <li key={id} style={{ backgroundColor: createRendomColor() }}>
-            <span className="label">{label}</span>
-            <span className="percentage">{percentage}</span>
+            <span>{label}</span>
+            <span>{percentage}</span>
           </li>
         ))}
       </StatList>
